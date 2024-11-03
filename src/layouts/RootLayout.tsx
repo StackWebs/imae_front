505013ab317d813962436ac59833be7fc1db4175
Marc Got <gotcritgmarc@gmail.com>
2024-11-03 20:37:38 +0100

    Shadcn Install

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,18 +3,70 @@ import {Link, MemoryRouter, Outlet, Route, Routes} from "react-router-dom";
 import Login from "../pages/Auth/Login";
 import Expeditions from "../pages/Expeditions/Expeditions";
 
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "../ui/breadcrumb"
+import { Separator } from "../ui/separator"
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "../ui/sidebar"
+import {AppSidebar} from "../components/sidebar/app-sidebar";
+
+export const iframeHeight = "800px"
+
+export const description = "An inset sidebar with secondary navigation."
+
 
 const RootLayout = () => {
 
     return (
         <>
-            <Link to="expeditions" className="text-sm font-medium transition-colors hover:text-primary" >
-                expeditions
-            </Link>
-            <Link to="auth/login" className="text-sm font-medium transition-colors hover:text-primary" >
-                login
-            </Link>
-            <Outlet/>
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    <header className="flex h-16 shrink-0 items-center gap-2">
+                        <div className="flex items-center gap-2 px-4">
+                            <SidebarTrigger className="-ml-1" />
+                            <Separator orientation="vertical" className="mr-2 h-4" />
+                            <Breadcrumb>
+                                <BreadcrumbList>
+                                    <BreadcrumbItem className="hidden md:block">
+                                        <BreadcrumbLink href="#">
+                                            Building Your Application
+                                        </BreadcrumbLink>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator className="hidden md:block" />
+                                    <BreadcrumbItem>
+                                        <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                                    </BreadcrumbItem>
+                                </BreadcrumbList>
+                            </Breadcrumb>
+                        </div>
+                    </header>
+                    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                        <Link to="expeditions" className="text-sm font-medium transition-colors hover:text-primary" >
+                            expeditions
+                        </Link>
+                        <Link to="auth/login" className="text-sm font-medium transition-colors hover:text-primary" >
+                            login
+                        </Link>
+                        <Outlet/>
+                        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                            <div className="aspect-video rounded-xl bg-muted/50" />
+                            <div className="aspect-video rounded-xl bg-muted/50" />
+                            <div className="aspect-video rounded-xl bg-muted/50" />
+                        </div>
+                        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+                    </div>
+                </SidebarInset>
+            </SidebarProvider>
         </>
     );
 }
