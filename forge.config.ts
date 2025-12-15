7bb174db8fffd0a0f392855de029a537881cd11e
Marc Got <gotcritgmarc@gmail.com>
2025-12-15 10:47:14 +0100

    mac os build

--- forge.config.ts
+++ forge.config.ts
@@ -7,6 +7,7 @@ import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-nati
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import { MakerDMG } from '@electron-forge/maker-dmg';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
@@ -17,7 +18,14 @@ const config: ForgeConfig = {
     icon: './src/assets/logo',
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [
+      new MakerSquirrel({}),
+    new MakerZIP({}, ['darwin']),
+    new MakerRpm({}),
+    new MakerDeb({}),
+    new MakerDMG({
+      format: 'ULFO',
+    }),],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
